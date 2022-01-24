8d11da39f4c37d6874d74bb97d3ff7a8b167840c
Your Name <you@example.com>
2022-01-24 08:29:01 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <mete name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+            <title>Cindy Kristiara Hadi | Home</title>
+    </head>
+    <body>
+        <h1> Halaman Home </h1>
+    </body>    
+</html>
